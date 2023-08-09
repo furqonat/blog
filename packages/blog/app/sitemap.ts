94c186db8dc9b78519cdf806c3dd885db479bd2b
furqonat <furqonr.atthoriq@gmail.com>
2023-08-09 14:54:43 +0700

    dynamic page to home

--- packages/blog/app/sitemap.ts
+++ packages/blog/app/sitemap.ts
@@ -1,10 +1,21 @@
+import { PrismaClient } from "@prisma/client";
 import { MetadataRoute } from "next";
-
-export default function Sitemap(): MetadataRoute.Sitemap {
-
+const prisma = new PrismaClient()
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+    const post = await prisma.post.findMany({ where: { status: true } })
+    const pages = [{
+        url: 'https://byfurqon.com/post',
+        lastModified: new Date(),
+    }, {
+        url: 'https://byfurqon.com/about',
+        lastModified: new Date(),
+    }, {
+        url: 'https://byfurqon.com',
+        lastModified: new Date(),
+    }]
+    const posts = post.map((item) => { return { url: `https://byfurqon.com/post/${item.slug}`, lastModified: new Date() } })
     return [
-        {
-            url: 'me.com'
-        }
+        ...pages,
+        ...posts,
     ]
 }
