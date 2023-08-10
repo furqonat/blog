647890b6413751ee10c50f5e9e12bc6311188e1b
furqonat <furqonr.atthoriq@gmail.com>
2023-08-10 10:58:12 +0700

    Revert "delete database url"
    This reverts commit e8147a24ad86bf702decd2b66d1a3bdd5281923d.

--- packages/manager/app/api/post/route.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
-export async function POST(request: Request) {
-  const { title, content, status } = request.body
-
-
-}
