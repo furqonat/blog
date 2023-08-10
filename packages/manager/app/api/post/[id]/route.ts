5b61b59c39ed74cd8b85f00e2c7e7d1227e4b75a
furqonat <furqonr.atthoriq@gmail.com>
2023-08-10 16:13:28 +0700

    improve performance for update post

--- packages/manager/app/api/post/[id]/route.ts
+++ packages/manager/app/api/post/[id]/route.ts
@@ -1,93 +1,76 @@
 import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 type CategoryBody = {
-  name: string
-}
+  name: string;
+};
+
 export async function PUT(request: Request, params: { params: { id: string } }) {
-  const { title, slug, content, status, categories } = await request.json()
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: params.params.id
-    },
-    include: {
-      categories: true
-    }
-  })
-  // if category is not undefined or null
-  console.log(categories)
+  const { title, slug, content, status, categories } = await request.json();
+
+  const updateData: any = {
+    title: title ?? undefined,
+    slug: slug ?? undefined,
+    content: content ?? undefined,
+    status: status,
+  };
+
+  // Update categories
   if (categories) {
-    // delete all connected category
-    await prisma.category.deleteMany({
+    const existingCategories = await prisma.category.findMany({
       where: {
-        post: {
-          every: {
-            id: params.params.id,
-          }
-        }
-      }
-    })
-    // loop all category body
-    for (const category of (categories as CategoryBody[])) {
-      // search all category that equal to category.name
-      const data = await prisma.category.findFirst({
-        where: {
-          name: {
-            equals: category.name
-          }
-        }
-      })
-      // if category is null we create new category and connect to post
-      if (!data) {
-        await prisma.post.update({
-          where: {
-            id: params.params.id
-          },
-          data: {
-            categories: {
-              create: {
-                name: category.name
-              }
-            }
-          }
-        })
-        // outerwise we connect the category
+        name: {
+          in: (categories as CategoryBody[]).map(category => category.name),
+        },
+      },
+    });
+
+    const categoryIdMap = new Map();
+
+    for (const existingCategory of existingCategories) {
+      categoryIdMap.set(existingCategory.name, existingCategory.id);
+    }
+
+    const categoryConnectIds: string[] = [];
+
+    for (const category of categories as CategoryBody[]) {
+      if (categoryIdMap.has(category.name)) {
+        categoryConnectIds.push(categoryIdMap.get(category.name));
       } else {
-        await prisma.post.update({
-          where: {
-            id: params.params.id
-          },
+        const newCategory = await prisma.category.create({
           data: {
-            categories: {
+            name: category.name,
+            post: {
               connect: {
-                id: data.id
-              }
-            }
-          }
-        })
+                id: params.params.id,
+              },
+            },
+          },
+        });
+
+        categoryIdMap.set(category.name, newCategory.id);
+        categoryConnectIds.push((newCategory.id));
       }
     }
+
+    updateData.categories = {
+      set: categoryConnectIds.map(id => ({ id })),
+    };
   }
 
+  // Update post
   const result = await prisma.post.update({
     where: {
-      id: params.params.id
+      id: params.params.id,
     },
-    data: {
-      title: title ? title : post.title,
-      slug: slug ? slug : post.slug,
-      content: content ? content : post.content,
-      status: status,
-    }
-  })
-
-  console.log(result.status)
+    data: updateData,
+  });
 
   if (!result) {
-    return NextResponse.json({ message: "Error while updating a data" })
+    return NextResponse.json({ message: "Error while updating data" });
   }
 
-  return NextResponse.json({ message: "Sukses update data", result: result.id })
+  return NextResponse.json({ message: "Success update data", result: result.id });
 }
