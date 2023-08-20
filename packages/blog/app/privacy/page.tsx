07f6149c8b33fa0a96cde6c26cff43595eb5b6b7
furqonat <furqonr.atthoriq@gmail.com>
2023-08-20 10:55:47 +0700

    add privacy policy

---
+++ packages/blog/app/privacy/page.tsx
@@ -0,0 +1,83 @@
+export default function PrivacyPage() {
+  return (
+    <main className={'container mx-auto'}>
+      <article className={'prose'}>
+        <h1>Privacy Policy</h1>
+        <p>
+          This Privacy Policy explains how ByFurqon.com  (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;) collects,
+          uses, and discloses information from visitors (&quot;you&quot; or &quot;users&quot;) of byfurqon.com (the &quot;Blog&quot;). We are
+          committed to protecting your privacy and ensuring the security of your personal information. By accessing or
+          using the Blog, you agree to the terms outlined in this Privacy Policy.
+        </p>
+        <h2>Information We Collect</h2>
+        <p>
+          We collect information from you when you visit and interact with our Blog. The information we collect may
+          include:
+        </p>
+
+        <ul>
+          <li>
+            <strong>Log Data</strong>: When you access the Blog, our server may automatically log certain information,
+            including your IP address, browser type, referring website, pages visited, and the date and time of your
+            visit.
+          </li>
+          <li>
+            <strong>Cookies and Similar Technologies</strong>: We use cookies and similar technologies to track user
+            preferences, enhance your experience on the Blog, and gather information about your usage patterns. You can
+            adjust your browser settings to refuse cookies or notify you when cookies are being sent.
+          </li>
+          <li>
+            <strong>Analytics</strong>: We gather non-personalized data about the usage of our Blog through analytics
+            tools. This data includes anonymized information about page views, clicks, and other interactions. Analytics
+            help us understand how users interact with our content and improve the overall user experience.
+          </li>
+        </ul>
+
+        <h2>Use of Information</h2>
+        <p>We use the collected information, including personal information if provided, for the following purposes:</p>
+        <ul>
+          <li>To provide and maintain the Blog&apos;s functionality.</li>
+          <li>To analyze and improve the Blog&apos;s content, features, and user experience.</li>
+          <li>To understand user preferences and trends based on aggregated and anonymized data.</li>
+          <li>To communicate with users, respond to inquiries, and provide support.</li>
+          <li>To comply with legal obligations.</li>
+        </ul>
+        <h2>Data Sharing and Disclosure</h2>
+        <p>
+          We do not sell, trade, or transfer your personally identifiable information to third parties. However, we may
+          share non-personalized, aggregated data with third parties for analytical purposes. We may also disclose
+          information if required by law or to protect our rights and interests.
+        </p>
+
+        <h2>Data Security</h2>
+        <p>
+          We implement reasonable security measures to protect your personal information from unauthorized access,
+          alteration, disclosure, or destruction. However, please be aware that no method of transmission over the
+          internet or electronic storage is completely secure.
+        </p>
+        <h2>Links to Third-Party Sites</h2>
+        <p>
+          The Blog may contain links to third-party websites. We are not responsible for the privacy practices or
+          content of these websites. We encourage you to review the privacy policies of any third-party sites you visit.
+        </p>
+
+        <h2>Changes to this Privacy Policy</h2>
+        <p>
+          We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any
+          significant changes by posting the updated policy on the Blog. Your continued use of the Blog after such
+          modifications will constitute your acknowledgment and acceptance of the updated policy.
+        </p>
+        <h2>Contact Us</h2>
+
+        <p>
+          If you have any questions about this Privacy Policy or your interactions with the Blog, please contact us at
+          danixml31@gmail.com.
+        </p>
+        <p>
+          By using the Blog, you agree to the terms outlined in this Privacy Policy. If you do not agree with the terms,
+          please do not access or use the Blog.
+        </p>
+      </article>
+    </main>
+  )
+}
---
+++ packages/blog/app/skills/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function SkillPage() {
+
+    return (
+        <main>
+            <h1></h1>
+        </main>
+    )
+}
